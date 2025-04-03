a01adf923c89704cea03845d6b45afa46cc4dbc2
Amaninreal <amaninreallife03@gmail.com>
2025-04-03 13:49:01 +0530

    adding codes

--- src/main/java/com/code/dsa/DsaPractice/practice/CompressString.java
+++ src/main/java/com/code/dsa/DsaPractice/practice/CompressString.java
@@ -16,9 +16,32 @@ public class CompressString {
         return compress.toString();
     }
 
+    public static String uniqueCompressString(String str){
+        StringBuilder map = new StringBuilder();
+        char currentChar = str.charAt(0);
+        int count = 1;
+        for(int i = 1; i < str.length(); i++){
+            if(str.charAt(i)== currentChar){
+                count++;
+            }
+            else {
+                if(count == 1){
+                    map.append(currentChar);
+                }
+                currentChar = str.charAt(i);
+                count = 1;
+            }
+        }
+        if(count == 1){
+            map.append(currentChar);
+        }
+        return map.toString();
+    }
+
     public static void main(String[] args) {
-        String str = "aaabbbccccdddd";
+        String str = "aaabbbccccabcaddddd";
         System.out.println(compressCount(str));
+        System.out.println(uniqueCompressString(str));
     }
 }
 
